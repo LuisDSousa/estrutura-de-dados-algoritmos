627d51e30940da95c782abbac1c3e73805e5d973
LuisDSousa <luisfelipesc@unipam.edu.br>
2025-08-26 01:16:51 +0000

    Banco01

---
+++ Banco-de-Dados/exemplos/banco01/schema.sql
@@ -0,0 +1,47 @@
+PRAGMA foreing_keys = ON;
+
+--Tabela usuario
+CREATE TABLE usuario(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    senha TEXT NOT NULL
+);
+
+-- Tabela cliente
+CREATE TABLE cliente(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    telefone TEXT,
+    usuario_id INTEGER NOT NULL UNIQUE, 
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+-- Tabela produto
+CREATE TABLE produto(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    descricao TEXT,
+    preco REAL NOT NULL CHECK (preco >= 0)
+);
+
+-- Tabela venda
+CREATE TABLE venda(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    data TEXT NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    cliente_id INTEGER NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE SET NULL,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id) ON DELETE SET NULL
+);
+
+--Tabela  de venda e produtos
+CREATE TABLE venda_produto(
+    venda_id INTEGER NOT NULL,
+    produto_id INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL CHECK (quantidade > 0),
+    preco_unitario REAL NOT NULL CHECK (preco_unitario >= 0),
+    PRIMARY KEY (venda_id, produto_id),
+    FOREIGN KEY (venda_id) REFERENCES venda(id) ON DELETE CASCADE
+    FOREIGN KEY (produto_id) REFERENCES produto(id) ON DELETE CASCADE
+);
